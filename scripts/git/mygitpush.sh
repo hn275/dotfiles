cfcf6857f8607c7ea09eff64836eed77dee76901
Hal <hn.nguyen275@gmail.com>
2022-04-09 00:25:58 -0700

    Delete scripts/git directory

--- scripts/git/mygitpush.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# Push git repositories  using PAT
-# April 4 2022
-# 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
-
-PAT='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'
-
-git push https://${PAT}@github.com/halnguyen/${1}.git
