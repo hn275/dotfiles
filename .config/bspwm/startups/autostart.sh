c2eb0f00745981100b0a258c1bec4a67879ad11f
Hal <hn.nguyen275@gmail.com>
2022-07-21 01:39:12 -0700

    update july 21, monochrome polybar

--- .config/bspwm/startups/autostart.sh
+++ .config/bspwm/startups/autostart.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/bash
 
 # Polybar
-usrpolybar
+usrpolybar & > /dev/null
 
 # Nightshift?
 if pgrep -x redshift > /dev/null;then
-	        polybar-msg action "#redshift.hook.0"
-	else
-	        polybar-msg action "#redshift.hook.1"
+	polybar-msg action "#redshift.hook.0" & 1> /dev/null
+else
+	polybar-msg action "#redshift.hook.1" & 1> /dev/null
 fi
 
 # Picom
 if ! pgrep -x picom > /dev/null;then
-	picom --experimental-backend &
+	picom --experimental-backend & 1> /dev/null
 fi
 
 if ! pgrep -x batsignal > /dev/null;then
-	usrbatsignal &
+	usrbatsignal & 1> /dev/null
 fi
+
+# Refresh all poly bar ipc
+usrkbdlight & > /dev/null
+usrbrightness & > /dev/null
