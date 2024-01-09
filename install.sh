44203c86332f118b7deb34d198ec2ca6950d4c51
Hal <haln_01@proton.me>
2024-01-09 02:39:56 -0800

    chore: i3 update

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/local/bin/bash
+
+echo "Installing dependencies"
+sudo pacman -Sy i3-wm libpulse brightnessctl \
+    nitrogen keyd make \
+    fish zsh alacritty neovim tmux
+
+echo "Coping config files"
+cp -r .config/* $HOME/.config/
+cp -r .tmux .tmux.conf .Xmodmap $HOME/
+echo "Done"
+
+echo "Setting up i3 status"
+echo "Done"
