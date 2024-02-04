c84f81eef4d4062d5b039010496805deb8a7e590
Hal <haln_01@proton.me>
2024-02-04 01:16:56 -0800

    feat: install scripts

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/usr/local/bin/bash
 
-echo "Installing dependencies"
-sudo pacman -Sy i3-wm libpulse brightnessctl \
-    nitrogen keyd make \
-    fish zsh alacritty neovim tmux \
-    slop shotgun \ # screenshotting dependencies
+#echo "Installing dependencies"
+#sudo pacman -Sy i3-wm libpulse brightnessctl \
+    #    nitrogen keyd make \
+    #    fish zsh alacritty neovim tmux \
+    #    slop shotgun \ # screenshotting dependencies
+#
+#echo "Coping config files"
+#cp -r .config/* $HOME/.config/
+#cp -r .tmux .tmux.conf .Xmodmap $HOME/
+#echo "Done"
+#
+#echo "Setting up i3 status"
+#echo "Done"
+#
+#echo "setting up environments"
 
-echo "Coping config files"
-cp -r .config/* $HOME/.config/
-cp -r .tmux .tmux.conf .Xmodmap $HOME/
-echo "Done"
+mkdir -p "$XDG_CONFIG_HOME/redshift/"
+ln -P ./.config/redshift/redshift.conf $XDG_CONFIG_HOME/redshift/redshift.conf
 
-echo "Setting up i3 status"
-echo "Done"
+ln -P ./.zshrc $HOME/.zshrc
